a413cedff373419fbd046d44511a08b2c8bd0830
Hany Samy <hs1547@fayoum.edu.eg>
2025-01-18 18:51:51 +0200

    add second version of edit password logic

--- TicTacToeServer/src/utilities/Codes.java
+++ TicTacToeServer/src/utilities/Codes.java
@@ -13,5 +13,7 @@ public class Codes {
     
     public static int REGESTER_CODE = 1;
     public static int CHANGE_PASSWORD_CODE = 3;
+    public static int SELECT_DATA_FOR_EDIT_PROFILE_CODE = 4;
+    
     
 }
